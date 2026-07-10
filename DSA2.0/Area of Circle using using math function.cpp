58fa693893b6e054dc96d3d5daef0c87522af35b
Aatir Ali <aatirali18@gmail.com>
2026-07-10 22:40:50 +0530

    More questions

---
+++ DSA2.0/Area of Circle using using math function.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int r;
+    cin >> r;
+
+    float c = 3.14 * pow(r, 2);
+
+    cout << "Area of circule: " << c << endl;
+
+    return 0;
+}
---
+++ DSA2.0/Area of Circle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int r;
+    cin >> r;
+
+    float c = 3.14 * r * r;
+
+    cout << "Area of circule: " << c << endl;
+
+    return 0;
+}
