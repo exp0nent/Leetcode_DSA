aff1b2f67c71da288e8f2c02fa86d57b028d647c
Aatir Ali <aatirali18@gmail.com>
2026-07-13 22:34:59 +0530

    Remove last digit

---
+++ DSA2.0/Remove Last digit.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long n;
+    cout << "Enter the digit: ";
+    cin >> n;
+    long digit = n/10;
+    cout << "Last digit remove: "<< digit << endl;
+}
