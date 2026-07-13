fa1a88ada7a67d9cc5481390fe1221b76211d7b3
Aatir Ali <aatirali18@gmail.com>
2026-07-13 21:08:58 +0530

    Last digit of a number

---
+++ DSA2.0/Last Digit of a Number.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long n;
+    cout << "Enter the digit: ";
+    cin >> n;
+
+    long last_digit = n % 10;
+
+    cout << last_digit;
+
+    return 0;
+}
