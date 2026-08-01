f94022513d271db0dff40562d1f3f5462d45c1cd
Aatir Ali <aatirali18@gmail.com>
2026-08-01 15:40:45 +0530

    Hollow Rectangle

---
+++ DSA2.0/Hollow Rectangle..cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int row, column;
+    cout<<"Enter the row: ";
+    cin>>row;
+    cout<<"Enter the column: ";
+    cin>>column;
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=column; j++){
+            if(i==1 || i==row){
+                cout<<"* ";
+            }
+            else if(j==1 || j== column){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
