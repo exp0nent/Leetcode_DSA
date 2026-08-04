ad0c914874dd37fd9c3f54592f85d462c1b0c7be
Aatir Ali <aatirali18@gmail.com>
2026-08-04 20:05:04 +0530

    Reverse pyramid

---
+++ DSA2.0/reverse pyramid_2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        // Print spaces
+        for (int j = 1; j <= i - 1; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int k = 1; k <=  2*(n-i)+1; k++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
