051dafbcbc8dc919d771319f9d6153e6b9af748b
Aatir Ali <aatirali18@gmail.com>
2026-08-22 13:49:32 +0530

    function:

---
+++ DSA2.0/main Function.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*Basic structure of a function
+returnType functionName(parameters) {
+    // code
+    return value;
+}*/
+
+int square(int n){
+    return n*n;
+}
+
+int main(){
+    cout<<square(5)<< endl;
+    cout<<square(12)<< endl;
+    return 0;
+}
