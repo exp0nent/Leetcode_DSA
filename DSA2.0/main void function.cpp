615b5f865a8976fdd062945783d5f2b82fad820d
Aatir Ali <aatirali18@gmail.com>
2026-08-22 13:59:37 +0530

    Void function

---
+++ DSA2.0/main void function.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*Basic structure of a function
+returnType functionName(parameters) {
+    // code
+    return value;
+}*/
+void hello(){
+    cout<<"Hello!";
+}
+
+
+int main(){
+    hello();
+    return 0;
+}
