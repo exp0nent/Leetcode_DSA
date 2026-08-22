546f60bbaca1c2e7af56d476106bd13dd541353f
Aatir Ali <aatirali18@gmail.com>
2026-08-22 14:53:47 +0530

    Function with parameters

---
+++ DSA2.0/main Function with parameters.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*Basic structure of a function
+returnType functionName(parameters) {
+    // code
+    return value;
+}*/
+void printnumber(int n){
+    cout<<n;
+}
+
+
+int main(){
+    printnumber(10);
+    return 0;
+}
