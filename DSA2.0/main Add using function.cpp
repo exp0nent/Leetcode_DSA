7a91af522f5015b9b3abb2782b668801a111b43c
Aatir Ali <aatirali18@gmail.com>
2026-08-22 13:54:19 +0530

    Add function

---
+++ DSA2.0/main Add using function.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*Basic structure of a function
+returnType functionName(parameters) {
+    // code
+    return value;
+}*/
+int add(int a, int b){
+    return a+b;
+}
+
+int main(){
+    int result = add(10,20);
+    cout<<result;
+    return 0;
+}
