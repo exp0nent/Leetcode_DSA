a5c1fb6670b41b42fcc41533661d01eaa3eef126
Aatir Ali <aatirali18@gmail.com>
2026-08-26 13:23:20 +0530

    Diamond question

---
+++ DSA2.0/Dimond problem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    // ===== Upper half: Pyramid =====
+    for (int i = 1; i <= n; i++) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+
+        // Print stars
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    // ===== Lower half: Reverse Pyramid =====
+    for (int i = n - 1; i >= 1; i--) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+
+        // Print stars
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
